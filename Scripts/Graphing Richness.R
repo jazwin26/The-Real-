8a117691bd6484623da4e1db21f4bcb610673634
jazwin26 <57026941+jazwin26@users.noreply.github.com>
2020-07-08 16:40:45 -0700

    Update Graphing Richness.R

--- Scripts/Graphing Richness.R
+++ Scripts/Graphing Richness.R
@@ -1,3 +1,9 @@
+#Graphgin Richness
+#Jazmyn Winzer 
+#jiwinzer@email.arizona.edu 
+#2020-7-8
+
+#Here we are loading in that data and needed packages for graphing and such
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
@@ -6,14 +12,20 @@ iNat <- read_csv("iNaturalist.csv")
 Andy <- read_csv("eBut_ah.csv")
 Az <- read_csv("eBut_az.csv")
 
+#Now we have to determine the different types of species we see across all sites
+#This is divided by our data sources
 unique_iNat <- unique(iNat$Species, incomparables = FALSE)
 unique_Andy<- unique(Andy$Species, incomparables = FALSE)
 unique_AZ <- unique(Az$Species, incomparables = FALSE)
 
+#Above we had data frame/list of all the unique species
+#Then we counted how many names appeared in each list
 iNat_total <- length(unique_iNat)
 Tohono_Chul_total <- length(unique_Andy)
 Tucson_total <- length(unique_AZ)
 
+#Then we created a new data frame of one variable (total number of names per list)
+#Then we cleaned up the data frame we made 
 Richness <- data.frame(iNat_total, Tohono_Chul_total, Tucson_total)
 
 Richness_Data_Frame <- 
@@ -23,9 +35,10 @@ Richness_Data_Frame <-
     "Tohono Chul" = Tohono_Chul_total,
     "eBut" = Tucson_total)
 
-Richness_Data_Frame
+#Richness_Data_Frame
 
-
+#We then converted our single row data frame into a three row data frame
+#Then we made a bar chart out of our new data frame 
 working_df <- data.frame(Site = c("iNat","Tohono Chul","eBut"),
                          Total = c(79, 64, 61))
 
